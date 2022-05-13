1c30edc897572cc6f9685742a3dc302b4482ce43
coldblade2000 <diegogranada7@gmail.com>
2022-05-13 14:03:22 -0500

    Entrega

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import JobsList from "./components/jobslist";
+import {IntlProvider} from "react-intl";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const getLocale = () => {
+    const locale =  navigator.language || navigator.userLanguage
+    console.log("Locale: " + locale)
+    return locale
+}
+
+ReactDOM.render(<IntlProvider locale={getLocale()} messages={getLocale().indexOf("es") === -1 ? localeEnMessages: localeEsMessages}>
+    <JobsList locale={getLocale()}/>
+</IntlProvider>, document.getElementById("root"));
